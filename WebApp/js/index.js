da500d4746438182c73d2e811e67f8d0be54e098
AGrieco96 <grieco.1715585@studenti.uniroma1.it>
2024-05-07 16:16:32 +0200

    revamped file path

--- WebApp/js/index.js
+++ WebApp/js/index.js
@@ -1,29 +1,35 @@
 // ----------- CONSTANT & VARIABLE ------------
 const labels_temp = [];
-const my_data= {
-    labels: labels_temp,
-    datasets: [{
-        label: 'Value',
-        pointBackgroundColor: 'rgb(0,0,0)',
-        borderWidth: 1,
-        fill: false,
-        tension: 0.3,
-        data: [],
-    }]  
-}
-var my_chart;
 
+var current_state = "2"
 // ----------- INITIALIZATION ------------
-function chart_layout(){
-
-  // New graph with Char js
-  my_chart = new Chart(document.getElementById("myChart"), {
-      type: 'bar',
-      data: my_data
+function chart_layout() {
+  Plotly.newPlot('myChart', [], {
+    title: '',
+    xaxis: {
+      title: 'Time'
+    },
+    yaxis: {
+      title: 'Alarm',
+      range: [0,1], // Imposta il range dell'asse y
+      tickvals: [0.50, 1], // Valori delle linee sull'asse y
+      ticktext: ['', ''], // Testo vuoto per nascondere le etichette
+    },
+    showlegend: false, // Nascondi la legenda
+    margin: { t: 50 }, // Imposta il margine superiore per la visualizzazione del titolo
   });
-
 }
 // ----------- HELPER FUNCTION -----------
+function handleTimeStamp(timestamp){
+  // Generate timestamp from $(timestamp()) of aws.
+  var seconds = Math.floor((timestamp / 1000) % 60);
+  var minutes = Math.floor((timestamp / (1000 * 60)) % 60);
+  var hours = Math.floor((timestamp / (1000 * 60 * 60)) % 24);
+  return hours.toString().padStart(2, '0') + ":" +
+          minutes.toString().padStart(2, '0') + ":" +
+          seconds.toString().padStart(2, '0');
+}
+
 // Lambda Function : read_value
 async function read_valueLambdaF(){
   try {
@@ -37,9 +43,11 @@ async function read_valueLambdaF(){
       const data = await response.json();
 
       console.log('Lambda function, read_value from DynnamoDB, invocation successful!');
-      console.log(data);
-      console.log(data[3].payload.M.Distance)
+      //console.log(data);
+      //console.log(data[3].payload.M.Distance)
       data.forEach((elem)=>{
+        //console.log(elem)
+        
         // Transform the response in a pretty way (This change based on the specific of AWS IoT Core Rule used to write on DynamoDB )
         const transformedPayload = {
           id: elem.id.N,
@@ -47,22 +55,33 @@ async function read_valueLambdaF(){
             Object.entries(elem.payload.M).map(([key, value]) => [key, value.S])
           )
         };
+        console.log(transformedPayload)
         var timestamp = transformedPayload.id
-        //var formattedTime = handleTimeStamp(timestamp);
-        console.log(trasnformedPayload.id)
+        var formattedTime = handleTimeStamp(timestamp);
+        //console.log(formattedTime)
+        //console.log(transformedPayload.id)
         
         
-        if(!(data_temp.labels.includes(formattedTime))){
-          var temp = parseFloat(transformedPayload["value"])
-          
-          
-          labels_temp.push(formattedTime);
-          data_temp.datasets[0].data.push(temp);
-        }
+        var trace = {
+          x: [formattedTime], // Tempo sull'asse x
+          y: [0.5], // Valore fisso sull'asse y per il pallino rosso
+          mode: 'markers', // Modalità punti
+          marker: {
+            color: transformedPayload["State"] === "1" ? 'rgb(255,0,0)' : 'rgb(0,0,0)', // Colore del pallino (rosso se lo stato è 1, altrimenti nero)
+            size: 20 // Dimensione del pallino
+          },
+        };
 
+        labels_temp.push(formattedTime); // Aggiungi il tempo all'array di etichette
+        Plotly.addTraces('myChart', [trace]); // Aggiungi il punto al grafico
+        //Plotly.addTraces('myChart', data);
+        //my_chart.update();
 
       });
-
+      labels_temp.sort(); // Ordina le etichette temporalmente
+      Plotly.relayout('myChart', { 'xaxis.categoryorder': 'array', 'xaxis.categoryarray': labels_temp }); // Ordina l'asse x
+      
+      /*
       labels_temp.sort((a, b) => {
         if (a > b) {
           return -1;
@@ -72,9 +91,8 @@ async function read_valueLambdaF(){
         }
         return 0;
       });
-      tempChart.update();
-      exec_measurements();
-      
+      */
+
    
     } else {
       console.error('Lambda function invocation failed with status:', response.status);
@@ -84,28 +102,21 @@ async function read_valueLambdaF(){
   }
 
 }
-
-function exec_measurements(){
-
-  const values = data_temp.datasets[0].data;
-  /*
-  const max_temp = Math.max(...values);
-
-  const min_temp = Math.min(...values);
-
-  const sum_temp = values.reduce((acc, val) => acc + val, 0);
-  const avg_temp = sum_temp / values.length;
-
-  const variation = max_temp - min_temp;
-  */
-
-  //display to html
-  document.getElementById("AVG_temp").innerHTML = "AVG : inserire valore "
-  document.getElementById("MAX_temp").innerHTML = "MAX : inserire valore"
-  document.getElementById("MIN_temp").innerHTML = "MIN : inserire valore" 
-  document.getElementById("delta_temp").innerHTML = "Variation in last Hour: inserire valore"// + variation + "&degC"
-
+function update_status(current){
+  switch(current){
+    case "0":
+      current_state="SMART ALARM"
+      break; 
+    case "1":
+      current_state="SMART LOCK"
+      break;
+    case "2":
+      current_state="OFF"
+      break;
+  }
+  document.getElementById("current_state").innerHTML = "Current State : " + current_state 
 }
+
 // ----------- MAIN FUNCTION ------------
 function main(){
 
@@ -114,7 +125,8 @@ function main(){
   chart_layout();
   console.log("Start - Retrieve info for the graph")
   read_valueLambdaF();
-
+  console.log(current_state)
+  update_status(current_state)
   /*
   // Starting campling data - ogni 10 secondi.
   setInterval(read_valueLambdaF, 10000)
@@ -130,7 +142,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Lambda Function : publish_to_iotcore	
 async function postLambdaFunction(content) {
-
+  update_status(content)
   await fetch("https://mzmk5uzj2fjrah5cqlcynm3ome0swley.lambda-url.eu-west-1.on.aws/", {
     method: 'POST',
     body: content,
